1578bbe09258308b276b7cfa5f79c735826083cb
EthanSouza <100168803+EthanSouza@users.noreply.github.com>
2023-05-10 16:12:59 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+import InitialScreen from "./screens/InitialScreen"
+
+export default function App(){
+
+  return(
+    <InitialScreen />
+  )  
+
+}
+
